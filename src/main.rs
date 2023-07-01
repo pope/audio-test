9c937b11467dd4c2b6c4b01d109c50a601307275
K. Adam Christensen <pope@shifteleven.com>
2023-07-01 16:27:36 -0700

    Use "contains(M8)" for check.
    This is because the Windows name I have doesn't report back M8 as a
    prefix.

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,6 @@ use cpal::platform::Device;
 use cpal::traits::{DeviceTrait, HostTrait};
 use std::error;
 
-// fn get_m8_input() -> Option<Device> {
-// 	cpal::available_hosts()
-// 		.into_iter()
-// 		.flat_map(|host_id| match cpal::host_from_id(host_id) {
-// 			Ok(res) => Some(res),
-// 			Err(_) => None,
-// 		})
-// 		.flat_map(|host| host.devices().ok())
-// 		.flatten()
-// 		.find(|device| {
-// 			device.default_input_config().is_ok()
-// 				&& device.name().is_ok_and(|name| name.starts_with("M8"))
-// 		})
-// }
-
 fn get_m8_input() -> Option<Device> {
 	cpal::default_host()
 		.devices()
@@ -24,7 +9,7 @@ fn get_m8_input() -> Option<Device> {
 		.flatten()
 		.find(|device| {
 			device.default_input_config().is_ok()
-				&& device.name().is_ok_and(|name| name.starts_with("M8"))
+				&& device.name().is_ok_and(|name| name.contains("M8"))
 		})
 }
 
