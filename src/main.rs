652d18621bb0273cc8ee51d5763aa353b83407cc
K. Adam Christensen <pope@shifteleven.com>
2023-07-01 19:14:52 -0700

    Add error checking for the output config.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use cpal::platform::Device;
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use cpal::{InputCallbackInfo, OutputCallbackInfo, StreamError};
 use ringbuf::HeapRb;
-use std::error;
 
 fn get_m8_input() -> Option<Device> {
 	cpal::default_host()
@@ -15,35 +14,45 @@ fn get_m8_input() -> Option<Device> {
 		})
 }
 
-fn main() -> Result<(), Box<dyn error::Error>> {
+fn main() -> anyhow::Result<()> {
 	let m8_input = get_m8_input().expect("failed to find M8 input device");
 	let default_output = cpal::default_host()
 		.default_output_device()
 		.expect("failed to find default output device");
-
-	// cpal::default_host()
-	// 	.output_devices()
-	// 	.into_iter()
-	// 	.flatten()
-	// 	.for_each(|device| {
-	// 		if let Ok(name) = device.name() {
-	// 			println!("{:?}", name);
-	// 		}
-	// 		device
-	// 			.supported_output_configs()
-	// 			.into_iter()
-	// 			.flatten()
-	// 			.for_each(|config| {
-	// 				println!("    {:?}", config);
-	// 			})
-	// 	});
 
 	println!("Using input device: {:?}", m8_input.name()?);
 	println!("Using output device: {:?}", default_output.name()?);
 
 	let input_config: cpal::StreamConfig =
 		m8_input.default_input_config()?.into();
-	let latency_frames = (150.0 / 1_000.0) * input_config.sample_rate.0 as f32;
+
+	let output_config_supported = default_output
+		.supported_output_configs()
+		.into_iter()
+		.flatten()
+		.any(|config| {
+			config.channels() == input_config.channels
+				&& config.min_sample_rate() >= input_config.sample_rate
+				&& config.max_sample_rate() <= input_config.sample_rate
+		});
+	if !output_config_supported {
+		let mut msg = String::from("Unable to find output config\n\n");
+		msg.push_str("Found:\n");
+		default_output
+			.supported_output_configs()
+			.into_iter()
+			.flatten()
+			.map(|device| format!("    {:?}\n", device))
+			.for_each(|device| {
+				msg.push_str(&device);
+			});
+		msg.push_str("\nExpected:\n");
+		let input_config = format!("    {:?}\n", input_config);
+		msg.push_str(&input_config);
+		return Err(anyhow::Error::msg(msg));
+	}
+
+	let latency_frames = 0.150 * input_config.sample_rate.0 as f32;
 	let latency_samples =
 		latency_frames as usize * input_config.channels as usize;
 
@@ -58,11 +67,11 @@ fn main() -> Result<(), Box<dyn error::Error>> {
 
 	let input_data_fn = move |data: &[f32], _: &InputCallbackInfo| {
 		if data
-			.into_iter()
+			.iter()
 			.map(|&sample| producer.push(sample))
 			.any(|res| res.is_err())
 		{
-			eprintln!("output stream feel behind. need to increase latency");
+			eprintln!("output stream fell behind. need to increase latency");
 		}
 	};
 
@@ -78,15 +87,9 @@ fn main() -> Result<(), Box<dyn error::Error>> {
 			}
 		}
 		if fell_behind {
-			eprintln!("input stream feel behind. need to increase latency");
+			eprintln!("input stream fell behind. need to increase latency");
 		}
 	};
-
-	// Build streams.
-	println!(
-		"Attempting to build both streams with f32 samples and `{:?}`.",
-		input_config
-	);
 
 	let input_stream = m8_input.build_input_stream(
 		&input_config,
@@ -102,8 +105,10 @@ fn main() -> Result<(), Box<dyn error::Error>> {
 		None,
 	)?;
 
-	input_stream.play()?;
-	output_stream.play()?;
+	input_stream
+		.play()
+		.expect("unable to start M8 input stream");
+	output_stream.play().expect("unable to start output stream");
 
 	// Run for 3 seconds
 	println!("Playing for 3 seconds... ");
